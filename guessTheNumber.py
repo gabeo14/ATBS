749d9af8bd5841bdd5e35edbeedbaaae233a3fcc
Gabe Oleinik <gomusic14@gmail.com>
2022-08-18 16:38:51 -0400

    Made and debugged guess the number.

---
+++ guessTheNumber.py
@@ -0,0 +1,23 @@
+#guess the number game
+import random
+
+print('Hello. Welcome to guess the number. What\'s your name?')
+name = input()
+
+print('Well ' + name + ', I\'m thinking of a number between 1 and 20.')
+secretnumber = random.randint(1, 20)
+
+for guessesTaken in range(1, 7):
+    print('Take a guess.')
+    #print(secretnumber) #debug
+    guess= int(input())
+    
+    if guess < secretnumber:
+        print('Too low. Guess again.')
+    elif guess > secretnumber:
+        print('Too high. Guess again.')
+    else:
+        break
+
+print('Correct! the number is ' + str(secretnumber) + '!')
+print('You took ' + str(guessesTaken) + ' guesses.')
